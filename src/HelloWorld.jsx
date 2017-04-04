ed008786e84eb0b7936df20b9466756e5a0725e1
lukebelliveau <lbellive@thoughtworks.com>
2017-04-03 21:48:00 -0400

    Now, we are actually rendering a React element and mounting it to the DOM.  The magic happens in renderDOM.jsx, which imports a React element from HelloWorld.jsx, and uses react-dom's render() method to attach our Hello World message to the DOM.

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelloWorld = () => (
+  <div>Hello World!</div>
+);
+
+export default HelloWorld;
---
+++ src/renderDOM.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from "./HelloWorld";
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
