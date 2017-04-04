8ebaeb98229b2bb9fb086f36875f29547d2046dd
lukebelliveau <lbellive@thoughtworks.com>
2017-04-03 21:33:56 -0400

    Added basic webpack config for a react app, with entry point renderDOM.jsx. Webpack will begin here, and output a bundled file at dist/app.bundle.js.  This app.bundle.js is called inside a <script> tag inside index.html.  This is how we get React's javascript running inside our webpage.  Please note that at this point, Webpack isn't actually doing anything useful - renderDOM.jsx is currently empty, and we are still serving a static page with no React rendering. HOWEVER - you must run 'yarn webpack' here for the app to be bundled and served properly. The page will not load without running this command before starting your server.

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
+//serve static assets
+app.use(express.static(__dirname + '/dist'));
+
+//handle other assets (react stuff) through app.bundle.js script inside index.html
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, 'index.html'))
 });
