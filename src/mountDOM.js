e3d4fe9d3fca296925b1d008b165086e7d9a0d96
lukebelliveau <lbellive@thoughtworks.com>
2017-05-12 12:43:58 -0500

    first commit, sample app running in React

---
+++ src/mountDOM.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <Greeting />
+)
+
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  componentWillMount() {
+    this.setState({
+      name: 'Hey you'
+    })
+  }
+  render() {
+    return (
+      <div>
+        <NameBox onChange={(event) => this.updateName(event)}/>
+        <Compliment userName={this.state.name}/>
+      </div>
+    )
+  }
+}
+
+const NameBox = (props) => (
+  <input onChange={ props.onChange }></input>
+)
+
+const Compliment = ({ userName }) => (
+  <h1>
+    { userName }, you really are
+    something special. I can't even.
+    I love you!
+  </h1>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
