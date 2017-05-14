e82c0a4d7552f616759da358ec487ee90f7fffa6
lukebelliveau <lbellive@thoughtworks.com>
2017-05-14 12:23:58 -0400

    Blank slate. Pass those tests!!

--- src/Deact.js
+++ src/Deact.js
@@ -1,46 +1,16 @@
-/*
-* IGNORE THIS!
-* Helper class.
-*
-* Sorry!!
-* */
-class Component {constructor(element){
-  if(!element.type)
-    this.mount = () => {
-      const node = document.createElement('span');
-      node.innerHTML = element;
-      return node;
-    };
-}};
-
+//IGNORE THIS! helper class for string literals. Sorry!!
+class Component{constructor(element){if(!element.type)this.mount=()=>{const node = document.createElement('span');node.innerHTML = element;return node;};}};
 
 export class DOMComponent extends Component {
   constructor(element) {
     //for demo purposes only.
     super(element);
 
-    this.currentElement = element;
+
   }
 
   mount() {
-    const element = this.currentElement;
-    const props = element.props;
-    const node = document.createElement(element.type);
 
-    Object.keys(element.props).forEach(propName => {
-      if(propName !== 'children') node.setAttribute(propName, props[propName])
-    });
-
-    let children = props.children || [];
-    if(!Array.isArray(children)) children = [children];
-
-    children.forEach(childElement => {
-      const child = new DOMComponent(childElement);
-      const mountedChild = child.mount();
-      node.appendChild(mountedChild);
-    });
-
-    return node;
   }
 }
 
@@ -48,10 +18,7 @@ export class DOMComponent extends Component {
   Analogous to ReactDOM.render().
 */
 const render = (element, containerNode) => {
-  const domComponentInstance = new DOMComponent(element);
-  const node = domComponentInstance.mount();
 
-  containerNode.appendChild(node);
 };
 
 export default {
