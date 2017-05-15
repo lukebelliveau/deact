3472b71c48c4e40b617dcacd4eec4cc2054857fd
lukebelliveau <lbellive@thoughtworks.com>
2017-05-14 21:43:15 -0400

    broke the code a bit, to be fixed in 3.3

--- src/Deact.js
+++ src/Deact.js
@@ -21,7 +21,7 @@ export class DOMComponent {
 
     //instantiate, mount, and append child elements.
     children.forEach(child => {
-      const childInstance = instantiateComponent(child);
+      const childInstance = new DOMComponent(child);
       const mountedChild = childInstance.mount();
       node.appendChild(mountedChild);
     });
